af3726fbb927e381f733966052bc9557973b3632
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2019-05-25 19:22:31 -0300

    Add custom font to website

--- components/Page.jsx
+++ components/Page.jsx
@@ -1,9 +1,18 @@
+import Head from 'next/head'
 import Navigation from './Navigation'
 
 const Page = props =>
-  <div>
+  <div className='page component'>
+    <Head>
+      <link key='font-imports' href='https://fonts.googleapis.com/css?family=Heebo&display=swap' rel='stylesheet' />
+    </Head>
     <Navigation />
     {props.children}
+    <style jsx>{`
+    .page.component {
+      font-family: Heebo, sans-serif;
+    }
+    `}</style>
   </div>
 
 export default Page
