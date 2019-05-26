ec2a521ec0fc02a0c8e952e76a13546afbca4b2c
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2019-05-26 10:11:03 -0300

    feat: Read projects from github:gabrielchiconi and list them in Projects page

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,8 +1,46 @@
+import Octokit from '@octokit/rest'
+
 import Page from '../components/Page'
 
-const Projects = () =>
-  <Page title='Projetos'>
-    Projects page
-  </Page>
+class Projects extends React.Component {
+  static async getInitialProps () {
+    const octo = Octokit({ userAgent: 'lins.dev GitHub client' })
+    const { data: repoData } = await octo.repos.listForUser({ username: 'gabrielchiconi' })
+    const gitHubRepos = repoData.map(repo => ({
+      name: repo.name,
+      url: repo.html_url,
+      description: repo.description,
+      updatedAt: new Date(repo.updated_at),
+      stars: repo.stargazers_count,
+      forks: repo.forks,
+      homepage: repo.homepage,
+      language: repo.language,
+      isFork: repo.fork
+    }))
+    return { gitHubRepos }
+  }
+
+  render () {
+    return (
+      <Page title='Projetos'>
+        <h2>Meus projetos!</h2>
+        <ul>
+          {this.props.gitHubRepos.map(repo =>
+            <li>
+              <h3>
+                <a href={repo.homepage || repo.url} title={repo.name}>
+                  {repo.name}
+                </a>
+              </h3>
+              <p>
+                {repo.language && `[${repo.language}]`} {repo.description}
+              </p>
+            </li>
+          )}
+        </ul>
+      </Page>
+    )
+  }
+}
 
 export default Projects
