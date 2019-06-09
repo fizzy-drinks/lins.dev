8d65e43032178b00373fd30c56f08d412bd11a04
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2019-06-09 15:47:05 -0300

    feat: Update Contact page to be more high-res friendly

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,5 +1,6 @@
 import Page from '../components/Page'
 import PageHeader from '../components/PageHeader'
+import WidthContainer from '../components/WidthContainer'
 
 const Contact = () =>
   <Page title='Contato'>
@@ -7,22 +8,28 @@ const Contact = () =>
       <PageHeader.Title>Contato</PageHeader.Title>
       <PageHeader.Sub>Vamos tomar um café? Adoraria ouvir o que você tem a dizer.</PageHeader.Sub>
     </PageHeader>
-    <p>Essas são as formas pelas quais podemos conversar:</p>
-    <article>
-      <h3>Contate-me por e-mail.</h3>
-      <p>
-        É tradicional e prático.
-        Meu endereço principal é <a href='mailto:gabriel.chiconi@gmail.com'>gabriel.chiconi@gmail.com</a>,
-        e normalmente respondo a todas as mensagens dentro de um dia.
-      </p>
-    </article>
-    <article>
-      <h3>Me telefone ou envie uma mensagem.</h3>
-      <p>
-        Eu uso Telegram e WhatsApp rotineiramente e sempre atendo ou
-        retorno ligações. <a href='tel:+5513981523054'>Me adicione</a> e entre em contato!
-      </p>
-    </article>
+    <WidthContainer>
+      <p>Essas são as formas pelas quais podemos conversar:</p>
+    </WidthContainer>
+    <WidthContainer>
+      <article>
+        <h3>Contate-me por e-mail.</h3>
+        <p>
+          É tradicional e prático.
+          Meu endereço principal é <a href='mailto:gabriel.chiconi@gmail.com'>gabriel.chiconi@gmail.com</a>,
+          e normalmente respondo a todas as mensagens dentro de um dia.
+        </p>
+      </article>
+    </WidthContainer>
+    <WidthContainer>
+      <article>
+        <h3>Me telefone ou envie uma mensagem.</h3>
+        <p>
+          Eu uso Telegram e WhatsApp rotineiramente e sempre atendo ou
+          retorno ligações. <a href='tel:+5513981523054'>Me adicione</a> e entre em contato!
+        </p>
+      </article>
+    </WidthContainer>
   </Page>
 
 export default Contact
