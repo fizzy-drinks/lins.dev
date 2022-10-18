3146507768e1e83e137ed1d4db0dce623fa27f02
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2022-10-18 13:19:46 -0300

    SEO tweaks

--- components/pages/Rango.tsx
+++ components/pages/Rango.tsx
@@ -10,47 +10,45 @@ const RangoPage: FC = () => {
   const { t } = useTranslation();
 
   return (
-    <>
-      <MainContent>
-        <NextSeo
-          title={t('seo.title')}
-          description={t('seo.description')}
-          canonical={'https://lins.dev/rango'}
-          openGraph={{
-            url: 'https://lins.dev/rango',
-          }}
-          additionalLinkTags={[{ rel: 'icon', href: '/favicon.png' }]}
-        />
-        <main>
-          <figure className='text-center'>
-            <Image
-              src={RangoImage.src}
-              width={RangoImage.width}
-              height={RangoImage.height}
-              alt={t('pages.rango.title')}
-              className='rounded-md shadow-lg shadow-neutral-500'
-            />
-          </figure>
-          <h1 className='text-4xl text-neutral-500 mt-2'>
-            {t('pages.rango.title')}
-          </h1>
-          <p className='mt-2'>{t('pages.rango.description')} 🥪</p>
-          <nav className='text-center text-sm mt-2'>
-            <a href='https://rango.lins.dev'>
-              <TextLinkStyle>{t('cta.view')}</TextLinkStyle>
-            </a>{' '}
-            |{' '}
-            <a
-              href='https://github.com/fizzy-drinks/rango'
-              target='_blank'
-              rel='noreferrer'
-            >
-              <TextLinkStyle>{t('cta.source')}</TextLinkStyle>
-            </a>
-          </nav>
-        </main>
-      </MainContent>
-    </>
+    <MainContent>
+      <NextSeo
+        title={t('seo.title')}
+        description={t('seo.description')}
+        canonical={'https://lins.dev/rango'}
+        openGraph={{
+          url: 'https://lins.dev/rango',
+        }}
+        additionalLinkTags={[{ rel: 'icon', href: '/favicon.png' }]}
+      />
+      <main>
+        <figure className='text-center'>
+          <Image
+            src={RangoImage.src}
+            width={RangoImage.width}
+            height={RangoImage.height}
+            alt={t('pages.rango.title')}
+            className='rounded-md shadow-lg shadow-neutral-500'
+          />
+        </figure>
+        <h1 className='text-4xl text-neutral-500 mt-2'>
+          {t('pages.rango.title')}
+        </h1>
+        <p className='mt-2'>{t('pages.rango.description')} 🥪</p>
+        <nav className='text-center text-sm mt-2'>
+          <a href='https://rango.lins.dev'>
+            <TextLinkStyle>{t('cta.view')}</TextLinkStyle>
+          </a>{' '}
+          |{' '}
+          <a
+            href='https://github.com/fizzy-drinks/rango'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <TextLinkStyle>{t('cta.source')}</TextLinkStyle>
+          </a>
+        </nav>
+      </main>
+    </MainContent>
   );
 };
 
