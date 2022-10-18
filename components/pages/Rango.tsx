389ac83e6940fe2d91f2ece030b6749127eb462f
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2022-10-18 08:48:28 -0300

    Add links to project page

--- components/pages/Rango.tsx
+++ components/pages/Rango.tsx
@@ -1,8 +1,11 @@
 import { useTranslation } from 'next-i18next';
 import { NextSeo } from 'next-seo';
 import { FC } from 'react';
+import Image from 'next/image';
 import PageContainer from 'components/ui/PageContainer';
 import PageHeader from 'components/PageHeader';
+import TextLinkStyle from 'components/ui/TextLinkStyle';
+import RangoImage from 'assets/rango.png';
 
 const HomePage: FC = () => {
   const { t } = useTranslation();
@@ -21,12 +24,33 @@ const HomePage: FC = () => {
       <div className='flex flex-col justify-between grow basis-1'>
         <PageHeader />
         <figure>
-          <img src='/images/rango.png' alt='Rango' />
+          <Image
+            src={RangoImage.src}
+            width={RangoImage.width}
+            height={RangoImage.height}
+            alt={t('pages.rango.title')}
+            className='rounded-md shadow-lg shadow-neutral-500'
+          />
         </figure>
       </div>
-      <main className='w-[500px] text-left grow basis-1'>
-        <h1 className='text-4xl text-neutral-500 mt-2'>{t('pages.rango.title')}</h1>
+      <main className='w-[460px] text-left grow basis-1'>
+        <h1 className='text-4xl text-neutral-500 mt-2'>
+          {t('pages.rango.title')}
+        </h1>
         <p className='mt-2'>{t('pages.rango.description')} 🥪</p>
+        <nav className='text-center text-sm mt-2'>
+          <a href='https://rango.lins.dev'>
+            <TextLinkStyle>{t('cta.view')}</TextLinkStyle>
+          </a>{' '}
+          |{' '}
+          <a
+            href='https://github.com/fizzy-drinks/rango'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <TextLinkStyle>{t('cta.source')}</TextLinkStyle>
+          </a>
+        </nav>
       </main>
     </PageContainer>
   );
