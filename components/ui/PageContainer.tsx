f87a61c5cb8f939786c25dae03bc4d52e4a19808
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2022-10-21 07:34:46 -0300

    Remove unused class

--- components/ui/PageContainer.tsx
+++ components/ui/PageContainer.tsx
@@ -1,17 +1,8 @@
-import { clsx } from 'clsx';
 import { FC, PropsWithChildren } from 'react';
-import useTheme from 'hooks/useTheme';
 
 const PageContainer: FC<PropsWithChildren> = ({ children }) => {
-  const { darkMode } = useTheme();
-
   return (
-    <div
-      className={clsx(
-        'mx-auto px-3 flex flex-col items-center w-full h-full gap-3 lowercase dark:bg-gray-900 dark:text-gray-200',
-        darkMode && 'dark'
-      )}
-    >
+    <div className='mx-auto px-3 flex flex-col items-center w-full h-full gap-3 lowercase dark:bg-gray-900 dark:text-gray-200'>
       {children}
     </div>
   );
