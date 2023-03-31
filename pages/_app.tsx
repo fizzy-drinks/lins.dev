60e86a69482ff68f5f551e1ad0e085012fd03572
fizzy <fizzy@lins.dev>
2023-03-31 16:02:41 -0300

    Add GTM script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { appWithTranslation, SSRConfig } from 'next-i18next';
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 import { FC } from 'react';
 import PageLayout from 'components/PageLayout';
 import LastfmProvider, {
@@ -14,6 +15,13 @@ const LDApp: FC<
   return (
     <ThemeProvider darkMode={pageProps.darkMode}>
       <LastfmProvider recentTracks={pageProps.recentTracks}>
+        <Script id='gtm' strategy='afterInteractive'>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KG2J8HB');`}
+        </Script>
         {pageProps?.statusCode ? (
           <Component {...pageProps} />
         ) : (
